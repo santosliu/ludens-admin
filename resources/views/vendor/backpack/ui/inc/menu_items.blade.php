90c8e8efd18638f55a72b0594f0c87f6e894fd5e
santosliu <cragon@gmail.com>
2025-11-11 09:33:05 +0800

    添加 csrf token check

--- resources/views/vendor/backpack/ui/inc/menu_items.blade.php
+++ resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -1,4 +1,6 @@
 {{-- This file is used for menu items by any Backpack v6 theme --}}
+
+<meta name="csrf-token" content="{{ csrf_token() }}">
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 
 <x-backpack::menu-item title="字彙表" icon="la la-question" :link="backpack_url('proper-noun')" />
